5a178e778343ff889fd745f316e08e2b6b658ecc
Alex Wu <alex@tokbox.com>
2018-07-11 17:09:47 -0700

    modified the UI slightly and added name of active speaker in luker mode

--- public/js/lecture-lurker.js
+++ public/js/lecture-lurker.js
@@ -21,7 +21,7 @@ session.on({
 
       var options = {fitMode: 'contain', width: '100%', height: '100%', name: name + ' (Teacher)'};
       var teacher = document.getElementById('teacher');
-      session.subscribe(event.stream, teacher, options);
+      SpeakerDetection(session.subscribe(event.stream, teacher, options));
     } else {
       console.log("student");
           
@@ -34,7 +34,7 @@ session.on({
       document.getElementById('subscribers').appendChild(subContainer);
 
       // Subscribe to the stream that caused this event, put it inside the container we just made
-      session.subscribe(event.stream, options);
+      SpeakerDetection(session.subscribe(event.stream, options));
     }
 
       console.log("streamCreated");
@@ -46,6 +46,46 @@ session.on({
   }
 
 });
+
+var SpeakerDetection = function(subscriber) {
+  var activity = null;
+  var data = subscriber.stream.connection.data;
+  var name = data.substring(data.indexOf("name=") + 5);
+
+  subscriber.on('audioLevelUpdated', function(event) {
+    var now = Date.now();
+    if (event.audioLevel > 0.2) {
+      if (!activity) {
+        activity = {timestamp: now, talking: false};
+      } else if (activity.talking) {
+        activity.timestamp = now;
+      } else if (now - activity.timestamp > 1000) {
+        // detected audio activity for more than 1s
+        // for the first time.
+        activity.talking = true;
+        startTalking(name);
+      }
+    } else if (activity && now - activity.timestamp > 3000) {
+      // detected low audio activity for more than 3s
+      if (activity.talking) {
+        stopTalking(name);
+      }
+      activity = null;
+    }
+  });
+}
+
+var startTalking =  function(name) {
+  console.log(name + ": started talking");
+  var speaker = document.getElementById("speaker");
+  speaker.innerHTML = "<h2>Speaking: " + name + "</h2>";
+}
+
+var stopTalking = function() {
+  console.log(name + ": stopped talking");
+  var speaker = document.getElementById("speaker");
+  speaker.innerHTML = "<h2>Speaking: None</h2>";
+}
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
 session.connect(token);
